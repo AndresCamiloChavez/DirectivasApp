be4335be0682176b1b31d052c80f05186f48e234
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-07-28 21:13:28 -0500

    Formulario reactivo

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
@@ -6,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private form: FormBuilder) { }
+  miFormulario: FormGroup = this.form.group({
+    'nombre': ['', Validators.required]
+  })
+
 
   ngOnInit(): void {
+  }
+
+
+  tieneError(campo: string): boolean{
+    return this.miFormulario.get(campo)?.invalid ?? false;
   }
 
 }
